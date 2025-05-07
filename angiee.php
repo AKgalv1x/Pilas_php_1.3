74abc40e67f46cb7206d5d92a836ef176ff0d308
AKgalv1x <akgalvis@gmail.com>
2025-05-07 08:22:04 -0500

    Pilas_pHP

---
+++ angiee.php
@@ -0,0 +1,19 @@
+<?php
+  $bsd = array();
+
+  array_push($bsd,"Fukuzawa");
+  array_push($bsd,"Ranpo");
+  array_push($bsd,"Yosano");
+  array_push($bsd,"Kunikida");
+  array_push($bsd,"Dazai");
+  array_push($bsd,"Tanizaki");
+  array_push($bsd,"Naomi");
+  array_push($bsd,"Kenji");
+  array_push($bsd,"Atsushi");
+  array_push($bsd,"Kyouka");
+
+   $ADA_member = array_pop($bsd); 
+
+  echo "La última persona que ha entrado a la Agencia Armada de Detectives fue " .$ADA_member;
+
+?>
